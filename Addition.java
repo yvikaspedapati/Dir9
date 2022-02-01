faf36b60a2a3d1d8ae3272c78389c820f24760c7
yvikaspedapati <pedapatibabu@gmail.com>
2022-02-01 21:24:09 +0530

    modified Addition.java

---
+++ Addition.java
@@ -0,0 +1,16 @@
+class Addition
+{
+	public ststic void main(string args[])
+	{
+		int num1,num2,res;
+system.out.print1n("Enter first number value");
+scanner s = new scanner(system.in);
+	num1=s.nextInt();
+	system.out.print1n("Enter first number value");
+num2=s.nextInt();
+res=num1+num2;
+system.out.print1n("Result is"+res);
+	}
+}
+
+
